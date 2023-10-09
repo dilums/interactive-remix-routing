e106600ef6f58dd1eb899179e00484fee3a80b69
Valentin Baca <vbaca@indeed.com>
2023-10-09 12:37:03 -0700

    typo fix on errors docs

--- app/routes/errors/good.tsx
+++ app/routes/errors/good.tsx
@@ -5,7 +5,7 @@ export default function ErrorGood() {
     <div className="component">
       <div className="component-path">{filePath}</div>
       <div className="component-description">
-        The bad.tsx is designed to will break at runtime. The error will be
+        The bad.tsx is designed to break at runtime. The error will be
         caught by the nearest error boundary present up in the hierarchy
         (/routes/errors.tsx in this case).
       </div>
